58ea76c163be5f617727bb32aa1f8f0427a2bcee
Eileen Duffner <eduffner@wustl.edu>
2017-03-08 10:37:32 -0600

    add comments and init Parent class

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,12 +1,35 @@
 import media
 import fresh_tomatoes
 
-shawshank_redemption = media.Movie("Shawshank Redemption","Fear can hold you prisoner. Hope can set you free.","https://www.youtube.com/watch?v=6hB3S9bIaco","http://www.impawards.com/1994/posters/shawshank_redemption_ver2.jpg")
-saved = media.Movie("Saved","Got Passion? Get Saved. 5:28","https://www.youtube.com/watch?v=je18yGc6jXk","http://images2.fanpop.com/image/photos/14100000/Movie-Poster-saved-14124914-682-1023.jpg")
-brokeback_mountain = media.Movie("Brokeback Mountain","Love Is A Force Of Nature","https://www.youtube.com/watch?v=-xuugq7fito","http://www.impawards.com/2005/posters/brokeback_mountain.jpg")
-green_mile = media.Movie("Green Mile","Walk a mile you'll never forget.","https://www.youtube.com/watch?v=uDybmxbKf4Ys","http://www.impawards.com/1999/posters/green_mile_ver3_xlg.jpg")
-ten_things_i_hate_about_you = media.Movie("10 Things I Hate About You","How do I loathe thee? Let me count the ways.","https://www.youtube.com/watch?v=AWmjzCZr0Jw","http://abcnews.go.com/images/Entertainment/REX_10_things_cast_jtm_141117.jpg")
-finding_nemo = media.Movie("Finding Nemo","There are 3.7 trillion fish in the ocean. They're looking for one.","https://www.youtube.com/watch?v=wZdpNglLbt8","https://d35fkdjhhgt99.cloudfront.net/static/use-media-items/17/16316/full-900x1300/56702cc2/pva6ds.jpeg?resolution=0")
+shawshank_redemption = media.Movie("Shawshank Redemption",
+								   "Fear can hold you prisoner. Hope can set you free.",
+								   "https://www.youtube.com/watch?v=6hB3S9bIaco",
+								   "http://www.impawards.com/1994/posters/shawshank_redemption_ver2.jpg")
+
+saved = media.Movie("Saved",
+					"Got Passion? Get Saved. 5:28",
+					"https://www.youtube.com/watch?v=je18yGc6jXk",
+					"http://images2.fanpop.com/image/photos/14100000/Movie-Poster-saved-14124914-682-1023.jpg")
+
+brokeback_mountain = media.Movie("Brokeback Mountain",
+								 "Love Is A Force Of Nature",
+								 "https://www.youtube.com/watch?v=-xuugq7fito",
+								 "http://www.impawards.com/2005/posters/brokeback_mountain.jpg")
+
+green_mile = media.Movie("Green Mile",
+						 "Walk a mile you'll never forget.",
+						 "https://www.youtube.com/watch?v=uDybmxbKf4Ys",
+						 "http://www.impawards.com/1999/posters/green_mile_ver3_xlg.jpg")
+
+ten_things_i_hate_about_you = media.Movie("10 Things I Hate About You",
+										  "How do I loathe thee? Let me count the ways.",
+										  "https://www.youtube.com/watch?v=AWmjzCZr0Jw",
+										  "http://abcnews.go.com/images/Entertainment/REX_10_things_cast_jtm_141117.jpg")
+
+finding_nemo = media.Movie("Finding Nemo",
+						   "There are 3.7 trillion fish in the ocean. They're looking for one.",
+						   "https://www.youtube.com/watch?v=wZdpNglLbt8",
+						   "https://d35fkdjhhgt99.cloudfront.net/static/use-media-items/17/16316/full-900x1300/56702cc2/pva6ds.jpeg?resolution=0")
 
 movie_list = [shawshank_redemption, saved, brokeback_mountain, green_mile, ten_things_i_hate_about_you, finding_nemo]
 fresh_tomatoes.open_movies_page(movie_list)
--- media.py
+++ media.py
@@ -1,15 +1,27 @@
 import webbrowser
 
 class Media():
+	"""
+	This class stores media data such as title
+	"""
 	def __init__(self, title):
 		self.title = title
 
 class Movie(Media):
+	"""
+	This class stores movie data such as storyline, trailer, and movie poster
+	"""
 	def __init__(self, movie_title, movie_storyline, trailer_youtube_url, poster_image):
-		self.title = movie_title
+		"""
+		This method initializes and instance of the class Movie
+		"""
+		Media.__init__(movie_title)
 		self.storyline = movie_storyline
 		self.trailer_youtube_url = trailer_youtube_url
 		self.poster_image_url = poster_image
 
 	def show_trailer(self):
+		"""
+		This method opens a movie's trailer in a new browser window
+		"""
 		webbrower.open(self.trailer_url)
